1b9344d05f51425135c152c1b1e6a0f90fadd740
shreyas0924 <shreyasp0924@gmail.com>
2022-11-19 23:27:11 +0530

    foucs to input

--- useRef-hook/src/App.jsx
+++ useRef-hook/src/App.jsx
@@ -3,17 +3,21 @@ import "./App.css"
 
 function App() {
   const [name, setName] = useState("")
-  const renderCount = useRef(1)
+  const inputRef = useRef()
 
-  useEffect(() => {
-    renderCount.current = renderCount.current + 1
-  })
+  // useEffect(() => {
+  //   renderCount.current = renderCount.current + 1
+  // })
+
+  function focus(){
+    inputRef.current.focus()
+  }
 
   return (
     <div>
-      <input type={name} onChange={(e) => setName(e.target.value)} />
+      <input ref={inputRef} type={name} onChange={(e) => setName(e.target.value)} />
       <h1>My name is {name}</h1>
-      <h1>I rendered {renderCount.current} times</h1>
+      <button onClick={focus}>Focus</button>
     </div>
   )
 }
